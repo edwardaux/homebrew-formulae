b6c59da64965293ea08523f3c59e4c40d6d1b2ba
Craig Edwards <craig@blackdogfoundry.com>
2021-01-19 08:03:42 +1100

    Initial commit

---
+++ Formula/pipes.rb
@@ -0,0 +1,18 @@
+class Pipes < Formula
+  desc "Swift implementation of CMS Pipelines - stream-based record processing"
+  homepage "https://github.com/edwardaux/pipes"
+  url "https://github.com/edwardaux/pipes.git"
+  version "1.0.0"
+  sha256 ""
+  license "MIT"
+
+  depends_on :xcode => ["12.0", :build]
+
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pipe literal hello"
+  end
+end
