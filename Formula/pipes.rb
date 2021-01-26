2664f7a3cfc8a2e4269ca08f052dfb16b35f3aea
Craig Edwards <craig@blackdogfoundry.com>
2021-01-27 08:39:24 +1100

    Added tgz and sha256

--- Formula/pipes.rb
+++ Formula/pipes.rb
@@ -1,9 +1,9 @@
 class Pipes < Formula
   desc "Swift implementation of CMS Pipelines - stream-based record processing"
   homepage "https://github.com/edwardaux/pipes"
-  url "https://github.com/edwardaux/pipes.git"
+  url "https://github.com/edwardaux/Pipes/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 ""
+  sha256 "bacc05e9fcda7ec4fed435f5b8482f7d629c273746889e003a2212ca5be70fc0"
   license "MIT"
 
   depends_on :xcode => ["12.0", :build]
