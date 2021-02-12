c59a13ee724e9ac08576dbff7b96fd776e1e3ac9
Craig Edwards <craig@blackdogfoundry.com>
2021-02-12 20:09:12 +1100

    Bump to 1.1

--- Formula/pipes.rb
+++ Formula/pipes.rb
@@ -1,9 +1,9 @@
 class Pipes < Formula
   desc "Swift implementation of CMS Pipelines - stream-based record processing"
   homepage "https://github.com/edwardaux/pipes"
-  url "https://github.com/edwardaux/Pipes/archive/1.0.0.tar.gz"
+  url "https://github.com/edwardaux/Pipes/archive/1.1.0.tar.gz"
   version "1.0.0"
-  sha256 "bacc05e9fcda7ec4fed435f5b8482f7d629c273746889e003a2212ca5be70fc0"
+  sha256 "53b74deca4f52360dade646cf38ffd0ba85c1c49e2c4753c57ed5635e52e78f2"
   license "MIT"
 
   depends_on :xcode => ["12.0", :build]
